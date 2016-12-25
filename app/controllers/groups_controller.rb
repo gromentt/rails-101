901071880cbdb9752118ea484e627530a39d3da2
王文欣 <gromentt@gmail.com>
2016-12-25 08:17:49 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,7 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
  def show
    @group = Group.find(params[:id])
+    @posts = @group.posts
  end
 
  def edit
@@ -36,7 +37,7 @@ end
 end
 
  def destroy
-      
+
        @group.destroy
        flash[:alert] = "Group deleted"
        redirect_to groups_path
